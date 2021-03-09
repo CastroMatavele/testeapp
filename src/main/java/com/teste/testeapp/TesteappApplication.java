6a8f0d99dc8d6235102ecb7ffddd3d69ec59a59b
Castro <matavelecastrofaifane@gmail.com>
2021-03-09 08:31:14 +0200

    first

---
+++ src/main/java/com/teste/testeapp/TesteappApplication.java
@@ -0,0 +1,13 @@
+package com.teste.testeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteappApplication.class, args);
+	}
+
+}
